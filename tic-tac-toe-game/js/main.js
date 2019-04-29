58a2bf9896aa420221f1b3df99fce1f1d9f0176c
Forrest Smith <forrestsmith@Forrests-MacBook-Pro.local>
2019-04-29 01:56:47 -0700

    Finished, still have a little tinkering to do

--- tic-tac-toe-game/js/main.js
+++ tic-tac-toe-game/js/main.js
@@ -1,8 +1,8 @@
-/*----- constants -----*/ 
+/*----- constants -----*/
 const MARKERS = {
    '1': `<img src="images/X-lightcyan.png">`,
    '-1': `<img src="images/O-lightcyan.png">`,
-   'null': ''
+   'null': `<img src="images/blank.png">`
 };
 
 const PLAYERS = {
@@ -10,40 +10,46 @@ const PLAYERS = {
    '-1': 'WOPR'
 };
 
-/*----- app's state (variables) -----*/ 
-var board, winner, turn;
+/*----- app's state (variables) -----*/
+var board, trans, winner, turn;
 
-/*----- cached references -----*/ 
+/*----- cached references -----*/
 const msgEl = document.getElementById('msg');
-// msgEl.innerHTML = 'HELLO<br><br>A STRANGE GAME.<br>THE ONLY WINNING MOVE IS<br>NOT TO PLAY.<br><br>HOW ABOUT A NICE GAME OF CHESS?';
-/*----- event listeners -----*/ 
+const butEl = document.querySelector('button');
+/*----- event listeners -----*/
 document.querySelector('section').addEventListener('click', tictac);
 document.querySelector('button').addEventListener('click', init);
 
 /*----- functions -----*/
 init();
-
+const setTrans = () => trans = (board[0].map((col, i) => board.map(row => row[i])));
 function tictac(evt) {
-   const div = evt.target.id.match(/\d+/g).map(Number); // Outputs [row, col] of square clicked
-   const row = div[0]; // Row index
-   const col = div[1]; // Column index
-   const  move = board[row][col];
-   if (move || winner) return; // Disables marker change in prev. clicked square
-   console.log(div);
-   // winner = getWinner();
-   board[row][col] = turn;
-   turn *= -1;
+   const div = evt.target.parentElement.id.match(/\d+/g).map(Number);  // Outputs [row, col] of square clicked
+   if (board[div[0]][div[1]] || winner) return;          // Disables marker change in prev. clicked square and turn switch
+   board[div[0]][div[1]] = turn;                         // Array is updated with player value (1, -1) of clicked square
+   setTrans();
+   winner = getWinner();                                 // Sets winner var to value returned by getWinner()
+   turn *= -1;                                           // Turn switches
    render();
 }
 
-// GETWINNER
-// Winner will be 'T', null, or player, which can be retrieved from any idx of winning combo
 function getWinner() {
-   const transpose = board[0].map((col, i) => board.map(row => row[i]));
-   for (i = 0; i < board.length; i++) {
-      if (Math.abs(board[i].reduce((tot, sq) => tot + sq)) === 3);
-      // const sum = array.reduce((total, amount) => total + amount);
+   for (i = 0; i < board.length; i++) {                                          // Iterate, damn you!
+      if (Math.abs(board[i].reduce((x, y) => x + y)) === 3) {                // Horizontal check
+         return board[i][i];
+      } else if (Math.abs(trans[i].reduce((x, y) => x + y)) === 3) {       // Vertical check
+         return board[i][i];
+      }
    };
+   if (Math.abs(board[0][0] + board[1][1] + board[2][2]) === 3) {
+      return board[0][0];
+   } else if (Math.abs(board[2][0] + board[1][1] + board[0][2]) === 3) {      // Clean this up/ Ternary
+      return board[2][0];
+   } else if (board.flat().includes(null)) {
+      return null;
+   } else {
+      return 'T';
+   }
 }
 
 function render() {
@@ -53,52 +59,34 @@ function render() {
          div.innerHTML = MARKERS[colIdx];
       });
    });
-   // if (winner === 'T') {
-      //   msgEl.innerHTML = 'HELLO<br><br>A STRANGE GAME.<br>THE ONLY WINNING MOVE IS<br>NOT TO PLAY.<br><br>HOW ABOUT A NICE GAME OF CHESS?';
-      //   document.querySelector('button').innerHTML = `THERE IS NO WINNER<BR>PLAY AGAIN?`;
-      // } else if (winner) {
-         //    msgEl.innerHTML = 
-         //    document.querySelector('button').innerHTML = `${PLAYERS[turn]} HAS CHEATED<BR>PLAY AGAIN?`;
-         // } else {
-            //    msgEl.textContent = 'HELLO<br><br>A STRANGE GAME.<br>THE ONLY WINNING MOVE IS<br>NOT TO PLAY.<br><br>HOW ABOUT A NICE GAME OF CHESS?';
-            //    //   msgEl.innerHTML = `${PLAYERS[turn]}'s TURN`;
-            // }
-         }
-         
-         function init() {
-            board = [
-               [null, null, null],
-               [null, null, null],
-               [null, null, null],
-            ];
-            winner = null;
-            turn = 1;
-            render();
-         }
-         
-         /*
-         left  -  center  -  right
-         [0][0]   [0][1]     [0][2]
-         left  -  center  -  right
-         [1][0]   [1][1]     [1][2]
-         left  -  center  -  right
-         [2][0]   [2][1]     [2][2]
-         
-         Want: 
-         Mouseover border styling(or?)
-         Line through winning combo
-         LOGON: Joshua (case insensitive)
-WOPR , PROFESSOR FALKEN
-SHALL WE PLAY A GAME
-.wavs
+   if (winner == 'T') {
+      msgEl.innerHTML = 'HELLO<br><br>A STRANGE GAME.<br>THE ONLY WINNING MOVE IS<br>NOT TO PLAY.<br><br>HOW ABOUT A NICE GAME OF CHESS?';
+      document.querySelector('button').innerHTML = `THERE IS NO WINNER<BR>PLAY AGAIN?`;
+   } else if (winner) {
+      msgEl.innerHTML = 'HELLO<br><br>A STRANGE GAME.<br>THE ONLY WINNING MOVE IS<br>NOT TO PLAY.<br><br>HOW ABOUT A NICE GAME OF CHESS?';
+      document.querySelector('button').innerHTML = `${PLAYERS[turn * -1]} HAS CHEATED<BR>PLAY AGAIN?`;
+   } else {
+      msgEl.innerHTML = `${PLAYERS[turn]}'s TURN`;
+      document.querySelector('button').innerHTML = `PLAY AGAIN`;
+   };
+}
+     
+function init() {
+   board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+   ];
+   winner = null;
+   turn = -1;
+   render();
+};
+/*
+// Want:
+// Mouseover border styling(or?)
+// Line through winning combo
+// LOGON: Joshua (case insensitive)
+// .wavs
 
-images/O-lightcyan.png
-images/X-lightcyan.png
-
-LIGHT CYAN: #a8ffff
-✖ The HTML code is &#10006;.
-
-Also, google search: 'shall we play a game' ;^)
-document.getElementById("myImg").src = "http://www.google.com/image.png"; 
-var numbers = string.match(/\d+/g).map(Number);
+if (Math.abs(board.flat().reduce((tot, sq) => tot + sq)) === 9)
 */
